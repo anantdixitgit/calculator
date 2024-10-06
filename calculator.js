2bc7e9b2d1356dbdfcaae2d5393dd99a58ecd36a
anant_dixit_13 <anantdixit100@gmail.com>
2024-10-06 20:49:03 +0530

    changes

--- calculator.js
+++ calculator.js
@@ -4,22 +4,28 @@ function updatedvalue(){
     document.querySelector("#inputval").value=val;
 }
 
+const arr = ["1","2","3","4","5","6","7","8","9","0",".","+","-","*","/","=","C"]
+
 document.querySelector("#buttons").addEventListener('click',function(event){
-     const buttonval= event.target.getAttribute('data-value');
-     if(buttonval==='C'){
-        val='';
-     }
-     else if(buttonval==='='){
-        try {
-                    val = eval(val);  // Evaluate the expression
-                } catch (error) {
-                    val = 'invalid expression';  // Show error for invalid expressions
-                }
-     }
-     else{
-         val+=buttonval;
-     }
-     updatedvalue();
+    console.log(event.target.parentElement);
+    if(arr.includes(event.target.getAttribute('data-value'))) {
+
+        const buttonval= event.target.getAttribute('data-value');
+        if(buttonval==='C'){
+           val='';
+        }
+        else if(buttonval==='='){
+           try {
+                       val = eval(val);  // Evaluate the expression
+                   } catch (error) {
+                       val = 'invalid expression';  // Show error for invalid expressions
+                   }
+        }
+        else{
+            val+=buttonval;
+        }
+        updatedvalue();
+    }
 })
 
 
